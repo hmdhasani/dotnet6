86884c6ab2b6e2c945a1f090cbb9696571513cdb
Hamed Hasani <hmdhasani@hotmail.com>
2022-06-09 15:14:51 +0430

    webapi added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, Class!");
+﻿using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+var counter = 0;
+
+app.MapGet("/", () => {
+   return $"Hello, Class {counter++}!!"; 
+});
+
+app.Run();
