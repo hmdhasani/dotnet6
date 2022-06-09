f3d64b533128fc225282ad9281254b6118c06c23
Hamed <hmdhasani@hotmail.com>
2022-06-09 11:13:53 +0000

    add fact

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ var app = builder.Build();
 var counter = 0;
 
 app.MapGet("/", () => {
-   return $"Hello, Class {counter++}!!"; 
+   return $"Hello, World {counter++}!!"; 
+});
+
+app.MapGet("/fact", (int num) => {
+   ulong result = 1;
+
+   do
+   {
+      result *= (ulong)num;
+   } while(num-- > 1);
+
+   return result; 
 });
 
 app.Run();
